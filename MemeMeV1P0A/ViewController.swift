7dafe870d6334ceb701a2a4a281d80b54035ac02
FarhanaQazi <farhan.abu.qazi@att.net>
2018-07-23 20:32:55 -0400

    Initial Commit

---
+++ MemeMeV1P0A/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMeV1P0A
+//
+//  Created by Farhan Qazi on 7/23/18.
+//  Copyright © 2018 Farhan Qazi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
